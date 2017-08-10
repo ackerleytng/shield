83170e9bc7153a0f05917b8fb224195b44ca0dc5
Ackerley Tng <ackerleytng@gmail.com>
2017-08-10 23:16:31 +0800

    Testing with imported libraries
    Works with zipfile, but not with io.
    
    Looks like we have to look deeper into the implementation in io

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ def my_open(name, mode=None, buffering=None):
 import __builtin__
 saved_open = __builtin__.open
 __builtin__.open = my_open
+import os
+os.open = my_open
 
 
 print type(open("/dev/null", "r", -1))
+
+import io
+with io.open('spam.txt', 'w') as file:
+    file.write(u'Spam and eggs!')
+
+from zipfile import ZipFile
+with ZipFile('spam.zip', 'w') as myzip:
+    myzip.write('eggs.txt')
