c9647332a4655f080d7385993a237ad00fcb93a7
Ackerley Tng <ackerleytng@gmail.com>
2017-08-13 01:29:13 -0700

    Fixed test cases for windows

--- tests/test_common.py
+++ tests/test_common.py
@@ -6,16 +6,12 @@ from shield import common
     ("", ("", [])),
     ("foo", ("", ["foo"])),
     ("foo/", ("", ["foo"])),
-    ("foo\\", ("", ["foo\\"])),
-    ("\\foo", ("", ["\\foo"])),
     ("/", ("", ["/"])),
     ("foo/bar", ("", ["foo", "bar"])),
     ("/foo/bar", ("", ["/", "foo", "bar"])),
     ("/foo/bar/", ("", ["/", "foo", "bar"])),
     ("foo/bar/", ("", ["foo", "bar"])),
     ("/tmp/foo/bar.txt", ("", ["/", "tmp", "foo", "bar.txt"])),
-    ("c:foo", ("", ["c:foo"])),
-    ("//hostname/foo/bar.txt", ("", ["//", "hostname", "foo", "bar.txt"])),
 ])
 def test_split_path(in_, expected):
     assert common.split_path(in_) == expected
@@ -27,6 +23,17 @@ def test_split_path(in_, expected):
     (r"C:\Users\john\Desktop\foo\bar.txt", ("C:",
                                             ["\\", "Users", "john", "Desktop",
                                              "foo", "bar.txt"])),
+    ("c:", ("c:", [])),
+    ("c:/", ("c:", ["/"])),
+    ("c:/foo", ("c:", ["/", "foo"])),
+    ("c:\\", ("c:", ["\\"])),
+    ("c:/users/john/foo.txt", ("c:", ["/", "users", "john", "foo.txt"])),
+    ("c:foo", ("c:", ["foo"])),
+    ("//hostname/shared drive/bar.txt", ("//hostname/shared drive", ["/", "bar.txt"])),
+    (r"\\hostname\shared drive", (r"\\hostname\shared drive", [])),
+    ("\\foo", ("", ["\\", "foo"])),
+    # Windows sees this as empty paths after foo
+    ("foo\\", ("", ["foo"])),
 ])
 def test_split_path_windows(in_, expected):
     assert common.split_path(in_) == expected
@@ -40,6 +47,12 @@ def test_split_path_windows(in_, expected):
     ("c:\\", ("", ["c:\\"])),
     ("c:/foo", ("", ["c:", "foo"])),
     ("c:/users/john/foo.txt", ("", ["c:", "users", "john", "foo.txt"])),
+    # *nixes see this as a single file with : in the filename
+    ("c:foo", ("", ["c:foo"])),
+    # *nixes see this as a file with \ in the filename
+    ("\\foo", ("", ["\\foo"])),
+    # *nixes see this as an uncommon filename
+    ("foo\\", ("", ["foo\\"])),
 ])
 def test_split_path_non_windows(in_, expected):
     assert common.split_path(in_) == expected
