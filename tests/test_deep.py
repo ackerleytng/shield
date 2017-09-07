5094287f34647d8f9a2d6c255afe9459e2c6e11e
ackerleytng <ackerleytng@gmail.com>
2017-09-07 23:27:16 +0000

    Added shutil.move() testcases

--- tests/test_deep.py
+++ tests/test_deep.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import pytest
 import shutil
 import shield
+
 
 @pytest.mark.parametrize("base_path, expected_exception", [
     (shield.common.get_temp_path(), None),
@@ -29,3 +31,40 @@ def test_shutil_rmtree(base_path, expected_exception):
 
     if expected_exception:
         shutil.rmtree(path)
+
+
+@pytest.mark.parametrize("src_path, dst_path, expected_exception", [
+    (shield.common.get_temp_path(), shield.common.get_desktop_path(), 
+     None),
+    (os.path.expanduser("~"), shield.common.get_temp_path(), 
+     shield.common.ShieldError),
+    (shield.common.get_temp_path(), os.path.expanduser("~"), 
+     shield.common.ShieldError),
+    (os.path.expanduser("~"), os.path.dirname(sys.executable),
+     shield.common.ShieldError),
+])
+def test_shutil_move(src_path, dst_path, expected_exception):
+    src_path = os.path.join(src_path, "unique-nonexistent-directory")
+    os.mkdir(src_path)
+    
+    file_path = os.path.join(src_path, "unique-nonexistent-file.txt")
+    with open(file_path, 'w') as f:
+        f.write("test\n")
+
+    shield.install_hooks()
+    try:
+        if expected_exception:
+            with pytest.raises(expected_exception):
+                shutil.move(src_path, dst_path)
+        else:
+            shutil.move(src_path, dst_path)
+    except Exception:
+        traceback.print_exc()
+    finally:
+        shield.uninstall_hooks()
+
+    if expected_exception:
+        shutil.rmtree(src_path)
+    else:
+        final_path = os.path.join(dst_path, "unique-nonexistent-directory")
+        shutil.rmtree(final_path)
